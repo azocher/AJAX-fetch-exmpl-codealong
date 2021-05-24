0e0e10214dc8b2d01651e2bfae04ac3ea9a94e86
Anna Zocher <anna.zocher@gmail.com>
2021-05-24 15:22:08 -0700

    updated script js to include random user api call

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-console.log("This script is humming along...🦚")
+document.addEventListener("DOMContentLoaded", () => {
+    // store constant url for form dom element; request url
+    const requestUrl = "https://randomuser.me/api/?results="
+    let inputForm = document.querySelector("form")
+    let peopleList = document.querySelector("#peopleList")
 
-// GET request to reddit for kitten
-fetch("https://www.reddit.com/search.json?q=kittens")
-    .then((res) => {
-        console.log("it worked! 🎉")
-        return res.json()
+    // REQUEST DATA
+    // take form element and prevent default behavior
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        // get user inputed number
+        let userInput = input.value
+        
+        // make fetch request to const api url with given user number
+        fetch(requestUrl + userInput)
+            // .then --> take response data and format
+            .then((res) => {
+                console.log("Response came back!")
+            })
+            // .catch --> catch errors
+            .catch((err) => {
+                console.log(err)
+                return err
+            })
+
     })
-    .then((jsonDat) => {
-        jsonDat.data.children.forEach((elem) => {
-            console.log(elem.data.subreddit)
-        })
-        return jsonDat
-    })
-    .catch((err) => {
-        console.log(err)
-        return err
-    })
+        
+
+    // RESPONSE DATA
+    // collect formated data
+        // create an li element for each response
+        // add li element to DOM
+
+})
